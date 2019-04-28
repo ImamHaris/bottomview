e01e4dbab95905e264e15ff5d5699b1f183c671a
ImamHaris <iamimam0107@gmail.com>
2019-04-28 17:28:07 +0700

    Upload

---
+++ app/src/main/java/com/example/bottomview/MainActivity.java
@@ -0,0 +1,120 @@
+package com.example.bottomview;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+
+
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.example.bottomview.network.ApiServices;
+import com.example.bottomview.network.InitRetrofit;
+import com.example.bottomview.response.BeritaItem;
+import com.example.bottomview.response.ResponseBerita;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends FragmentActivity {
+
+    private RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        //I added this if statement to keep the selected fragment when rotating the device
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new HomeFragment()).commit();
+        }
+
+
+        setContentView(R.layout.fragmen_home);
+        // Inisialisasi Widget
+        recyclerView = (RecyclerView) findViewById(R.id.rvListBerita);
+        // RecyclerView harus pakai Layout manager
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        // Eksekusi method
+        tampilBerita();
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()) {
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.nav_post:
+                            selectedFragment = new PostFragment();
+                            break;
+                        case R.id.nav_user:
+                            selectedFragment = new UserFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
+
+
+
+
+
+
+    private void tampilBerita() {
+        ApiServices api = InitRetrofit.getInstance();
+        // Siapkan request
+        Call<ResponseBerita> beritaCall = api.request_show_all_berita();
+        // Kirim request
+        beritaCall.enqueue(new Callback<ResponseBerita>() {
+            @Override
+            public void onResponse(Call<ResponseBerita> call, Response<ResponseBerita> response) {
+                // Pasikan response Sukses
+                if (response.isSuccessful()){
+                    Log.d("response api", response.body().toString());
+                    // tampung data response body ke variable
+                    List<BeritaItem> data_berita = response.body().getBerita();
+                    boolean status = response.body().isStatus();
+                    // Kalau response status nya = true
+                    if (status){
+                        // Buat Adapter untuk recycler view
+                        AdapterBerita adapter = new AdapterBerita(MainActivity.this, data_berita);
+                        recyclerView.setAdapter(adapter);
+                    } else {
+                        // kalau tidak true
+                        Toast.makeText(MainActivity.this, "Tidak ada berita untuk saat ini", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBerita> call, Throwable t) {
+                // print ke log jika Error
+                t.printStackTrace();
+            }
+        });
+    }
+}
